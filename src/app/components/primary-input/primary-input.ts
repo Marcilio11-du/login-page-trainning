5d0a9bedecc41a8fb3fb5590d86422becf00b1bf
marcilio11-du <marcilio11du@gmail.com>
2026-01-20 00:21:56 +0100

    refactor: Solve the problem with the renderization of the atributtes for templates

--- src/app/components/primary-input/primary-input.ts
+++ src/app/components/primary-input/primary-input.ts
@@ -1,6 +1,6 @@
 import { InvokeFunctionExpr } from '@angular/compiler';
-import { Component, Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 @Component({
@@ -8,13 +8,46 @@ type InputTypes = "text" | "email" | "password"
   imports: [
     ReactiveFormsModule
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useClass: forwardRef(() => PrimaryInput),
+      multi: true
+    }
+  ],
   templateUrl: './primary-input.html',
   styleUrl: './primary-input.scss',
 })
-export class PrimaryInput {
+export class PrimaryInput implements ControlValueAccessor{
+
   @Input() type: InputTypes = "text";
-  @Input() formName: string = "";
   @Input() placeHolder: string = "";
   @Input() label: string = "";
+  @Input() inputName: string = "";
 
+  value: string = "";
+  
+  onChange: any = () => {}
+  onTouched: any = () => {}
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.onChange(input.value);
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // Implement if needed
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 }
