cfa0ba487bc9cb76893e26b2d4ca906f6ebac495
marcilio11-du <marcilio11du@gmail.com>
2026-01-20 10:19:26 +0100

    feat: Makes the logical to send datas to Back-end using LoginService

--- src/app/services/login.ts
+++ src/app/services/login.ts
@@ -1,8 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { LoginResponse } from '../types/login-response';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Login {
-  
+export class LoginService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  login(name: string, password: string) {
+    return this.httpClient.post<LoginResponse>("https://example.com/api/login", {name, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth_token", value.token);
+        sessionStorage.setItem("username", value.name);
+      })
+    );
+  }
+
 }
