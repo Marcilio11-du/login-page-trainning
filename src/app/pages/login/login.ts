d5961dada9a97753fef435bfecd99228b643a501
marcilio11-du <marcilio11du@gmail.com>
2026-01-20 09:43:40 +0100

    feat: Create the logical to send the data to Back-end with a testing URL

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,24 +2,52 @@ import { Component } from '@angular/core';
 import { DefaultLogin } from "../../components/default-login/default-login";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInput } from '../../components/primary-input/primary-input';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login';
+
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'app-login',
   imports: [
     DefaultLogin,
     ReactiveFormsModule,
-    PrimaryInput
+    PrimaryInput,
+  ],
+  providers: [
+    Login
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-  loginForm!: FormGroup
+  loginForm!: FormGroup<LoginForm>;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ) {
       this.loginForm = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required, Validators.minLength(6)])
       })
+  }
+
+  submitLogin() {
+    this.loginService.login(
+      this.loginForm.value.email,
+      this.loginForm.value.password
+    ).subscribe({
+      next: () => console.log("Login successful"),
+      error: () => console.log("Login failed")
+    }
+    )
+  }
+
+  navigate() {
+    this.router.navigate(["signup"]);
   }
 }
